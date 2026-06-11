f9f95b941aecf2a56659e111fe3bb257f22cf994
Julio Angulo <solojoas@gmail.com>
2026-06-11 22:16:38 +0200

    Added buffer as autocomplete feature
    Emulating vim autocomple ctrl+n/p with buffer option

--- init.lua
+++ init.lua
@@ -873,7 +873,7 @@ do
     },
 
     sources = {
-      default = { 'lsp', 'path', 'snippets' },
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
     },
 
     snippets = { preset = 'luasnip' },
